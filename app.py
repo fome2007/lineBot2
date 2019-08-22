5c016e5303a512e9879715ff0f2eb28bbe0af5d3
fome2007 <51429134+fome2007@users.noreply.github.com>
2019-08-22 08:58:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def callback():
          sendText(user,'แหวะ')
     else :
          sendText(user,'ห้ะ อะไรนะ')
-    return '',200ext)
+    return '',200
      
   def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
